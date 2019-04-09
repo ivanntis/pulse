61346a9efc029da48e6664019342a325fb978446
Ivan Peña <ipena@bancodebogota.com.co>
2019-04-09 09:43:17 -0500

    Se ajustan espacios del template

--- src/components/pulse-templates/flowtt/pulse-flowtt.tsx
+++ src/components/pulse-templates/flowtt/pulse-flowtt.tsx
@@ -17,14 +17,14 @@ export class PulseFlowtt {
     return (
       <div class="flowtt-main">
         <div class="flowtt-main--container pulse-grid">
-          <header class="row"> 
-            <div class="col-xs-12  col-lg-10 col-lg-offset-1">
+          <header class="row flowtt-main--container--header"> 
+            <div class="col-xs-12 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1">
               <slot name="header"></slot> 
             </div>
           </header>
-          <section class="row">
-            <div class="col-xs-12  col-lg-3 col-lg-offset-3 order-lg-2"> <slot name="summary"></slot> </div>
-            <div class="col-xs-12  col-lg-5 col-lg-offset-1 order-lg-1"> <slot name="content"></slot> </div>
+          <section class="row flowtt-main--container--content">
+            <div class="col-xs-12 col-md-10 col-lg-3  col-md-offset-1 col-lg-offset-2 order-lg-2"> <slot name="summary"></slot> </div>
+            <div class="col-xs-12  col-md-8 col-lg-5 col-md-offset-2 col-lg-offset-1 order-lg-1"> <slot name="content"></slot> </div>
           </section>
         </div>
       </div>
